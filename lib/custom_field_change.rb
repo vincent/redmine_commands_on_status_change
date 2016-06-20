8495d7f723d6a47c7001f6f728eccc0c85397164
Vincent Lark <vincent.lark@gmail.com>
2016-06-20 14:55:04 +0200

    also put status

--- lib/custom_field_change.rb
+++ lib/custom_field_change.rb
@@ -14,7 +14,7 @@ module RedmineCommandIssueStatusChange
       http = Net::HTTP.new(uri.host, uri.port)
       request = Net::HTTP::Post.new("/hooks/issue-changed")
       request.add_field('Content-Type', 'application/json')
-      request.body = URI.encode_www_form({'issue_id' => @issue.id})
+      request.body = URI.encode_www_form({'issue_id' => @issue.id, 'status' => @issue.status.name})
       response = http.request(request)
     end
   end
